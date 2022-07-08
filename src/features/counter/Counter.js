b463a714aa56243f6f8d013ce08b649553bdf996
yootaeyun <ew20@naver.com>
2022-07-09 01:48:34 +0900

    리덕스 툴킷 튜토리얼 챕터 1 (완료)

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -8,13 +8,17 @@ import {
 } from "./counterSlice";
 
 function Counter() {
+  // 화면에 출력할 count
   const count = useSelector(({ counter }) => counter.count);
   const dispatch = useDispatch();
 
+  // 인풋값 설정
   const [addCount, setAddCount] = useState(0);
 
+  // addCount가 숫자가 아니면 0으로 설정, 숫자면 해당 숫자 할당
   const addValue = Number(addCount) || 0;
 
+  // 0으로 리셋, addCount를 0으로 만들고, reset()을 호출시켜 {count}도 0으로 리셋
   const resetAll = () => {
     setAddCount(0);
     dispatch(reset());
@@ -23,6 +27,7 @@ function Counter() {
     <section>
       <p>{count}</p>
       <div>
+        {/* increment를 호출하여 1증가 */}
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(deincrement())}>-</button>
       </div>
